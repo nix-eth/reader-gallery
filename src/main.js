3e1cd789f0f57ca91a7fece9cb94b83de6610aa6
nix.eth <me@nix.art>
2023-11-07 07:47:56 -0800

    Add support for GA

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueGtag from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
@@ -8,5 +9,15 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+if (import.meta.env.VITE_GA_MEASUREMENT_ID !== undefined) {
+  app.use(
+    VueGtag,
+    {
+      config: { id: import.meta.env.VITE_GA_MEASUREMENT_ID }
+    },
+    router
+  )
+}
 
 app.mount('#app')
